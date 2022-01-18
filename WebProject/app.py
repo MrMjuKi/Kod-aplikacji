2351580ea07218a5e4d40acac7db594ee4008750
Piotr T <mrmjuki@gmail.com>
2022-01-18 23:40:33 +0100

    Wersja 0.1

---
+++ WebProject/app.py
@@ -0,0 +1,151 @@
+from flask import Flask, render_template, request, session
+from flask.templating import render_template_string
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+
+app = Flask(__name__)
+
+app.secret_key = 'klucz'
+  
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'admin'
+app.config['MYSQL_DB'] = 'daza_danych_ofert'
+  
+mysql = MySQL(app)
+
+def listaOfert(IDuzytkownika):
+    listaOfert = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    if IDuzytkownika != 'ANY':
+        listaOfert.execute('SELECT uzytkownik.login, oferta.nazwa_produktu, oferta.opis, oferta.ilosc_szt, oferta.cena, oferta.ID_oferty FROM oferta INNER JOIN uzytkownik ON oferta.ID_uzytkownika=uzytkownik.ID_uzytkownika WHERE oferta.ID_uzytkownika = % s', (str(IDuzytkownika)))
+    else:
+        listaOfert.execute('SELECT uzytkownik.login, oferta.nazwa_produktu, oferta.opis, oferta.ilosc_szt, oferta.cena FROM oferta INNER JOIN uzytkownik ON oferta.ID_uzytkownika=uzytkownik.ID_uzytkownika')
+
+    mysql.connection.commit()
+    listaOfert.close()
+    
+    return listaOfert
+
+@app.route('/')
+@app.route('/home')
+def index():
+    if session:
+        return render_template('index.html', listaOfert=listaOfert('ANY'))
+
+    return render_template('index.html', msg="Zaloguj sie w celu przegladania ofert")
+
+@app.route('/rejestracja', methods=['GET', 'POST'])
+def rejestracja():
+    if session:
+        return render_template('index.html',listaOfert=listaOfert('ANY'), msg ='Jestes juz zalogowany jako ' + str(session['username']))
+
+    if request.method == 'POST':
+        uzytkownik = request.form.get('inputUzytkownik')
+        email = request.form.get('inputEmail')
+        haslo = request.form.get('inputHaslo')
+
+        tworzenieKonta = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        tworzenieKonta.execute('BEGIN; INSERT INTO uzytkownik (login, haslo, emial) VALUES ( % s, % s, % s); INSERT INTO personalne_dane (ID_uzytkownika) VALUES (last_insert_id()); COMMIT;', (uzytkownik, haslo, email ))
+        #mysql.connection.commit
+        
+        return render_template('index.html',listaOfert=listaOfert('ANY') ,msg ='Rejestracja przebiegla pomyslnie. Teraz mozesz sie zalogowac')
+
+    return render_template('rejestracja.html')
+
+@app.route('/logowanie', methods=['GET', 'POST'])
+def logowanie():
+    if session:
+        return render_template('index.html' ,listaOfert=listaOfert('ANY'), msg ='Jestes juz zalogowany jako ' + str(session['username']))
+
+    if request.method == 'POST':
+        uzytkownik = request.form.get('inputUzytkownik')
+        haslo = request.form.get('inputHaslo')
+
+        probaLogowania = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        probaLogowania.execute('SELECT * FROM uzytkownik WHERE login = % s AND haslo = % s', (uzytkownik, haslo ))
+
+        konto = probaLogowania.fetchone()
+        if konto:
+            session['id'] = konto['ID_uzytkownika']
+            session['username'] = konto['login']
+            return render_template('index.html' ,listaOfert=listaOfert('ANY'), msg ='Udalo sie zalogowac. Witaj ' + session['username'])
+        else:
+            return render_template('index.html' ,listaOfert=listaOfert('ANY'), msg ='Nie udalo sie zalogowac. Wpisales zle haslo lub login.')
+
+    return render_template('logowanie.html')
+
+@app.route('/wyloguj')
+def wyloguj():
+    if not session:
+        return render_template('index.html' ,listaOfert=listaOfert('ANY'), msg ='Nie jestes zalogowany na zadne konto')
+
+    session.pop('id', None)
+    session.pop('username', None)
+
+    return render_template('index.html' ,listaOfert=listaOfert('ANY'), msg ='Wylogowanie przegieblo pomyslnie')
+
+@app.route('/dodajOferte', methods=['GET', 'POST'])
+def dodawanieOferty():
+    if not session:
+        return render_template('index.html' ,listaOfert=listaOfert('ANY'), msg ='Zaloguj sie lub utworz konto, by dodac oferte')
+
+    if request.method == 'POST':
+        nazwaProduktu = request.form.get('inputNameOfProduct')
+        opis = request.form.get('inputDescription')
+        ilosc = request.form.get('inputAmount')
+        cena = request.form.get('inputPrice')
+
+        dodanieOferty = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        dodanieOferty.execute('INSERT INTO oferta (ID_uzytkownika, nazwa_produktu, opis, ilosc_szt, cena) VALUES ( % s, % s, % s, % s, % s)', (session['id'], nazwaProduktu, opis, ilosc, cena ))
+
+        mysql.connection.commit()
+
+        return render_template('index.html' ,listaOfert=listaOfert('ANY'), msg ='Pomyslnie dodano produkt')
+
+    return render_template('/dodawanieoferty.html')
+
+@app.route('/mojeOferty', methods=['GET', 'POST'])
+def mojeoferty():
+    if not session:
+        return render_template('index.html' ,listaOfert=listaOfert('ANY'), msg ='Zaloguj sie, by zobaczyc swoje oferty')    
+    
+    if request.method == 'POST':
+        usunOferte = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        usunOferte.execute('DELETE FROM oferta WHERE ID_oferty = % s', (str(request.form.get('usun'))))
+
+        mysql.connection.commit()
+        return render_template('index.html' ,msg= 'Oferta zostala usunieta' ,listaOfert=listaOfert('ANY'))
+
+    return render_template('mojeOferty.html' ,listaOfert=listaOfert(session['id']))
+
+@app.route('/mojeDane', methods=['GET', 'POST'])
+def mojeDane():
+    if not session:
+        return render_template('index.html' ,listaOfert=listaOfert('ANY'), msg ='Zaloguj sie, by zobaczyc swoje dane')
+
+    if request.method == 'POST':
+        imie = request.form.get('inputName')
+        nazwisko = request.form.get('inputSurname')
+        telefon = request.form.get('inputNumber')
+
+        aktualizacjaDanych = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        aktualizacjaDanych.execute('UPDATE personalne_dane SET imie = % s, nazwisko = % s, nr_telefonu = % s WHERE ID_uzytkownika = % s', (imie, nazwisko, telefon, str(session['id']) ))
+
+        mysql.connection.commit()
+
+        return render_template('index.html' ,listaOfert=listaOfert('ANY'), msg ='Zmiany zapisano')
+
+
+    mojeDane = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+
+    mojeDane.execute('SELECT imie, nazwisko, nr_telefonu FROM personalne_dane WHERE ID_uzytkownika = % s', (str(session['id'])))
+    mysql.connection.commit()
+    mojeDane.close()
+    
+
+    return render_template('mojeDane.html', mojeDane=mojeDane)
+
+
+
+if __name__ == '__main__':
+    app.run('localhost', 4449)
